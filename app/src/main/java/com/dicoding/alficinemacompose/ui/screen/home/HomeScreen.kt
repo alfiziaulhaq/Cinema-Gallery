d4d53c952107f89df8160e6aed726a9ef44e8635
alfizu <alfiziaulhaq@gmail.com>
2024-01-07 23:41:32 +0700

    rename app

--- app/src/main/java/com/dicoding/alficinemacompose/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/dicoding/alficinemacompose/ui/screen/home/HomeScreen.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -20,8 +23,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.dicoding.alficinemacompose.R
 import com.dicoding.alficinemacompose.data.Injection
 import com.dicoding.alficinemacompose.data.ViewModelFactory
 import com.dicoding.alficinemacompose.model.BookCinema
@@ -29,7 +37,7 @@ import com.dicoding.alficinemacompose.ui.common.UiState
 import com.dicoding.alficinemacompose.ui.components.CinemaItem
 import com.dicoding.alficinemacompose.ui.components.SearchBar
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier = Modifier,
@@ -38,32 +46,54 @@ fun HomeScreen(
     ),
     navigateToDetail: (Long) -> Unit,
 ) {
-    viewModel.uiState.collectAsState(initial = UiState.Loading).value.let { uiState ->
-        when (uiState) {
-            is UiState.Loading -> {
-                viewModel.getAllCinema()
-            }
-
-            is UiState.Success -> {
-                HomeContent(
-                    book = uiState.data,
-                    modifier = modifier,
-                    navigateToDetail = navigateToDetail,
+    Column(
+        modifier = modifier.fillMaxSize()
+    ) {
+        CenterAlignedTopAppBar(
+            title = {
+                Text(
+                    text = stringResource(R.string.app_name),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp),
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Center
                 )
             }
+        )
+        viewModel.uiState.collectAsState(initial = UiState.Loading).value.let { uiState ->
+            when (uiState) {
+                is UiState.Loading -> {
+                    viewModel.getAllCinema()
+                }
 
-            is UiState.Error -> {
-                val TAG = "errors"
-                Log.d(TAG, "onFAILED: ${uiState.copy()}")
-                Column(modifier = modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally){
-                    Button(onClick = { viewModel.getAllCinema() },modifier=Modifier.padding(20.dp)) {
-                       Text(text = "Retry")
+                is UiState.Success -> {
+                    HomeContent(
+                        book = uiState.data,
+                        modifier = modifier,
+                        navigateToDetail = navigateToDetail,
+                    )
+                }
+
+                is UiState.Error -> {
+                    val TAG = "errors"
+                    Log.d(TAG, "onFAILED: ${uiState.copy()}")
+                    Column(
+                        modifier = modifier.fillMaxWidth(),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(
+                            onClick = { viewModel.getAllCinema() },
+                            modifier = Modifier.padding(20.dp)
+                        ) {
+                            Text(text = "Retry")
+                        }
                     }
                 }
             }
         }
     }
-
 }
 
 @OptIn(ExperimentalFoundationApi::class)
